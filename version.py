573408a915653cde7297d6e24a215739dbe1f85c
Patrick Johnson <patrick.l.johnson@gmail.com>
2013-03-07 20:03:09 -0800

    starting to implement command-line arguments

--- version.py
+++ version.py
@@ -1,7 +1,12 @@
-import sys
+#!/usr/bin/env python
 
-def readVersionNumber():
-    version = open("KVersion")
+import sys
+import argparse
+
+args = None
+
+def readVersionNumberFromFile():
+    version = open(args.version)
     versionDict = {}
     for line in version:
         key, value = line.split(" = ")
@@ -21,9 +26,28 @@ def readInPomXML():
     pom = open(outputFile, "w")
     pom.write(pomdotout)
 
+def parseCmdLine():
+    parser = argparse.ArgumentParser(description="Convert version numbers in <input> file, creat <output> file")
+
+    parser.add_argument('-i', '--input', default="pom.xml.in", help="Input File")
+    parser.add_argument('-o', '--output', default="pom.xml", help="Output File")
+    parser.add_argument('-s', '--version', default="KVersion", help="File for processing, default is yesterday.json")
+    parser.add_argument('-n', '--number', action='store_true', help="add hardcoded version number")
+
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-r', '--randomize', action='store_true', help='Randomize query types')
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    inputFile = sys.argv[1]
-    outputFile = inputFile.split(".in")[0]
-    newVersionNumber = readVersionNumber()
+    args = parseCmdLine()
+
+    inputFile = args.input
+    outputFile = args.output
+    newVersionNumber = None
+    if args.number:
+        newVersionNumber = "1.0.0" # args.number
+    else:
+        newVersionNumber = readVersionNumberFromFile()
+
     readInPomXML()
